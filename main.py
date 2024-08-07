9a578fe34fbae229b0499feba4709ad8514cae97
Roronoazoro2702 <rajharshit2702@gmail.com>
2024-08-07 10:16:14 +0530

    setting db models

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI, Path, Query
-from typing import Optional,List
-from typing import Union
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 from api import users, sections, courses
+from db.db_setup import engine
+from db.models import user, course
 
-app = FastAPI()
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Fast API LMS",
+    description="LMS for managing students and courses.",
+    version="0.0.1",
+    contact={
+        "name": "Zoro",
+        "email": "zoro@example.com",
+    },
+    license_info={
+        "name": "Chandigarh University",
+    },
+)
 
 app.include_router(users.router)
 app.include_router(sections.router)
